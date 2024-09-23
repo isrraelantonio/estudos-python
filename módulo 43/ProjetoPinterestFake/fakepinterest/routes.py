2ac8e00b9138ab8669761829d60acb04b42bcf16
isrraelantonio <israelantonio027@gmail.com>
2024-09-23 17:10:12 -0300

    atualização id para perfil e inserção de imagens.

--- módulo 43/ProjetoPinterestFake/fakepinterest/routes.py
+++ módulo 43/ProjetoPinterestFake/fakepinterest/routes.py
@@ -15,7 +15,7 @@ def homepage():
           usuario = Usuario.query.filter_by(email = formlogin.email.data).first()
           if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data): 
                login_user(usuario) 
-               return  redirect(url_for("perfil", usuario = usuario.username)) 
+               return  redirect(url_for("perfil",  id_usuario = usuario.id)) # devemos dentro de login também fazer essas mudanças para um direcionamento apartir do login.
                
 
      return render_template("homepage.html", form= formlogin) 
@@ -34,15 +34,21 @@ def criarconta():
           database.session.commit()
           
           login_user(usuario, remember = True)
-          return redirect(url_for("perfil", usuario = usuario.username))
+          return redirect(url_for("perfil", id_usuario = usuario.id)) # bo nossso redirecionamento estamos informando que o usuário não será mais selecionado pelo nome e sim pelo id, dentro da nossa tabela 
      return render_template("criarconta.html", form = formcriarconta)
 
 
 
-@app.route("/perfil/<usuario>")
+@app.route("/perfil/<id_usuario>") # Nesse caso fizemos a mudança em relação a quem nos referimos quando estamos buscando acessar o nosso usuário para nossa página de perfil. nesse caso fazemos uso do id_usuario pois  o id do mesmo é o único valor que define o usuário como único.
 @login_required  
-def perfil(usuario):
-     return render_template("perfil.html", usuario = usuario) 
+def perfil(id_usuario):
+     if int(id_usuario) == int(current_user.id): # estamos verificando se o usuário do perfil é o mesmo que está logado na conta agora pelo id
+          # Nesse caso o usuário estará vendo o seu própio perfil
+            return render_template("perfil.html", usuario = current_user) 
+     else:
+          # Nesse caso o usuário estará vendo o perfi de outro usuário.
+          usuario = Usuario.query.get(int(id_usuario))
+          return render_template("perfil.html", usuario = usuario) 
 
 
 
